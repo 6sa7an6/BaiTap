527cc10e1dd813ee145c81f9ef8b2b6578fa3713
VuVietAnh <6sa7an6@gmail.com>
2023-09-25 00:05:19 +0900

    add file

---
+++ 23-09-20-SS14/baithem2/index.js
@@ -0,0 +1,30 @@
+render = () => {
+    text = '';
+    for (let i = 1; i < 11; i++) {
+        text += `<li onclick = 'clickPoint(${i})'>${i}</li>`
+    }
+    document.getElementsByClassName('container__point__list')[0].innerHTML = `${text}`
+}
+render();
+clickPoint = (detail) => {
+    for (let i = 1; i < 11; i++) {
+        if (i == detail) {
+            document.getElementsByClassName('container__feedback__main__point')[0].innerHTML = `<p style = 'background-color : red'>${i}<p>`
+        }
+    }
+}
+let clickButton = document.getElementsByClassName('container__review__button')[0];
+clickButton.addEventListener('click', () => {
+    let review = document.getElementById('text').value
+    document.getElementsByClassName('container__feedback__main__review')[0].innerHTML = `<p>${review}</p>`
+})
+let clickEdit = document.getElementsByClassName('edit')[0];
+clickEdit.addEventListener('click', () => {
+    document.getElementById('text').value = '';
+    document.getElementsByClassName('container__feedback__main__review')[0].innerHTML = '';
+})
+let clickClose = document.getElementsByClassName('close')[0];
+clickClose.addEventListener('click', () => {
+    document.getElementsByClassName('container__feedback__main__review')[0].innerHTML = '';
+    document.getElementsByClassName('container__feedback__main__point')[0].innerHTML = '';
+})
